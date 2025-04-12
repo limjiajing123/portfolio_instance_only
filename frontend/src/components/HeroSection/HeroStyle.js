97009603c2929c9763673e6db302b18d38b7030c
limjiajing123 <LIMJ0236@e.ntu.edu.sg>
2025-04-12 23:55:08 +0800

    added contact so user can navigate easily to contact me

--- frontend/src/components/HeroSection/HeroStyle.js
+++ frontend/src/components/HeroSection/HeroStyle.js
@@ -92,23 +92,42 @@ export const HeroRightContainer = styled.div`
   }
 `;
 
+// export const Img = styled.img`
+//   position: relative;
+//   width: 100%;
+//   height: 100%;
+//   max-width: 400px;
+//   max-height: 400px;
+//   border-radius: 50%;
+//   border: 2px solid ${({ theme }) => theme.primary};
+
+//   @media (max-width: 768px) {
+//     max-width: 400px;
+//     max-height: 400px;
+//   }
+
+//   @media (max-width: 640px) {
+//     max-width: 280px;
+//     max-height: 280px;
+//   }
+// `;
+
 export const Img = styled.img`
   position: relative;
-  width: 100%;
-  height: 100%;
-  max-width: 400px;
-  max-height: 400px;
+  width: auto;
+  height: auto;
+  max-width: 450px;
+  max-height: 500px;
   border-radius: 50%;
+  object-fit: cover;
   border: 2px solid ${({ theme }) => theme.primary};
 
   @media (max-width: 768px) {
     max-width: 400px;
-    max-height: 400px;
   }
 
   @media (max-width: 640px) {
     max-width: 280px;
-    max-height: 280px;
   }
 `;
 
